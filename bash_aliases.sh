580a540de6f1cf4feada2503e3faae5ed136a0f8
eddie-m-m <eddie.m.menefee@gmail.com>
2023-10-09 15:10:25 -0700

    Update aliases

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -2,7 +2,7 @@
 alias cddf='cd ~/.dotfiles'
 alias c='clear'
 alias ll='ls -l'
-alias la='ls -A'
+alias la='ls -a'
 alias mkalias='vim ~/.dotfiles/bash_aliases.sh'
 alias rebash='source ~/.bash_profile'
 
