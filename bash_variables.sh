45c4ceb1bdc25d248db936b57801b3a288ca8b08
eddie-m-m <eddie.m.menefee@gmail.com>
2023-12-15 16:04:05 -0800

    Update bash_variables

--- bash_variables.sh
+++ bash_variables.sh
@@ -5,6 +5,6 @@ export BASH_SILENCE_DEPRECATION_WARNING=1
 # Initialize Path
 PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin
 export PATH
-PATH=$PATH:$HOME/.dotfiles/bin
+PATH=$PATH:$HOME/.dotfiles/bin:$HOME/Library/Python/3.9/bin
 
 eval "$(/opt/homebrew/bin/brew shellenv)"
