dc60ba64e3d7c1557812a06055abee2897d9b4b0
eddie-m-m <eddie.m.menefee@gmail.com>
2025-08-31 13:17:15 -0700

    rework

--- bashrc
+++ bashrc
@@ -1,5 +1,5 @@
-source ~/.dotfiles/bash/git-completion.sh
-source ~/.dotfiles/bash/git-prompt.sh
-source ~/.dotfiles/bash/bash_aliases.sh
-source ~/.dotfiles/bash/bash_variables.sh
-source ~/.dotfiles/bash/bash_prompt.sh
+source ~/.dotfiles/scripts/git-completion.sh
+source ~/.dotfiles/scripts/git-prompt.sh
+source ~/.dotfiles/scripts/bash_aliases.sh
+source ~/.dotfiles/scripts/bash_variables.sh
+source ~/.dotfiles/scripts/bash_prompt.sh
---
+++ scripts/bash_aliases.sh
@@ -0,0 +1,30 @@
+# general aliases
+alias cddf='cd ~/.dotfiles'
+alias c='clear'
+alias ll='ls -lh'
+alias la='ls -ah'
+alias lla='ls -lha'
+
+# program aliases
+alias py='python3'
+alias pip='pip3'
+alias pipupgrade='pip install --upgrade pip'
+alias pyut='python3 -m unittest'
+alias pyvenv='python3 -m venv'
+alias mkvenv='python3 -m venv venv'
+alias runvenv='. venv/bin/activate'
+alias gotest='go test -v'
+alias gobuild='go build -o'
+
+# git aliases
+alias com='git commit -m'
+alias gs='git status'
+alias lg="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+
+# config aliases
+alias rebash='source ~/.bashrc'
+alias mkalias='vim ~/.dotfiles/bash_scripts/bash_aliases.sh'
+
+alias hidclose='sudo chmod o-rw,g-rw /dev/hidraw*' 
+alias hidexpose='sudo chmod a+rw /dev/hidraw*'
+alias gethidinfo='bash ~/.dotfiles/scripts/displayhidrawinfo.sh'
