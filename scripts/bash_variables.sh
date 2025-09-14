0c86c6d64a76d649e58e50c18f414d7832e24b4f
eddie-m-m <eddie.m.menefee@gmail.com>
2025-09-14 10:09:39 -0700

    update path

--- scripts/bash_variables.sh
+++ scripts/bash_variables.sh
@@ -5,10 +5,10 @@ export VISUAL="$EDITOR"
 # Initialize Path
 PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin
 export PATH
-PATH=$PATH:$HOME/.dotfiles/bin:$HOME
+PATH=$PATH:$HOME/.dotfiles/bin:$HOME/.cargo/bin:$PATH
 if [[ "$OSTYPE" == "darwin"* ]]; then
     eval "$(/opt/homebrew/bin/brew shellenv)"
-    export EDITOR=nvim
+    export EDITOR=vim
     export BASH_SILENCE_DEPRECATION_WARNING=1
     export REDIS_URL=redis://localhost:6379/0
 fi
