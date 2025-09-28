bf05cd1a8f6fc204f68e45d59ab103f0cbbeb173
Eddie M. Menefee <eddie.m.menefee@gmail.com>
2025-09-27 17:40:30 -0700

    poll continuously for dynamic vpn icon in status bar

--- omarchy-configs/scripts/show-vpn-status.sh
+++ omarchy-configs/scripts/show-vpn-status.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-VPN_NAME=$(nmcli -t -f NAME,TYPE connection show --active | grep -E ':vpn|:wireguard' | cut -d ':' -f 1)
+update_vpn_status() {
+    VPN_NAME=$(nmcli -t -f NAME,TYPE connection show --active | grep -E ':vpn|:wireguard' | cut -d ':' -f 1)
 
-if [ -n "$VPN_NAME" ]; then
-    printf '{"text": " ", "tooltip": "Connected to: %s", "class": "vpn-connected"}\n' "$VPN_NAME"
-else
-    printf '{"text": "󰦏", "tooltip": "VPN Disconnected", "class": "vpn-disconnected"}\n'
-fi
+    if [ -n "$VPN_NAME" ]; then
+       printf '{"text": " ", "tooltip": "Connected to: %s", "class": "vpn-connected"}\n' "$VPN_NAME"
+    else
+       printf '{"text": "󰦏", "tooltip": "VPN Disconnected", "class": "vpn-disconnected"}\n'
+    fi
+}
+
+update_vpn_status
+
+stdbuf -oL nmcli monitor | while read -r line; do
+    update_vpn_status
+done
